c8939c8b1cd958de1fc4102febaeae6aab0ee0ac
sajid-ahsan-seyam-26 <sajidahsanseyamstuden>
2026-04-03 00:44:45 +0600

    update

--- 1.py
+++ 1.py
@@ -1,1 +1,25 @@
-print("hi")
+student=[]
+def add_student():
+    print("---------add student----------")
+    name=input("enter your name")
+    id=input ("enter your id")
+    roll=input("enter your roll")
+    department=input("enter your dpartment name")
+    phone=input("enter your phone number")
+
+    student={
+
+       "name": name,
+        "roll": roll,
+        "department": department,
+        "phone": phone
+
+
+    }
+    student.append(student)
+    print("your student added sucessfully")
+
+    def show_all_shoudent():
+        print("---all student print----")
+
+
